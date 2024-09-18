a47c08ab390830fff82d7ba0b344e76bc720aec2
felipethecosta <felipethecosta@gmail.com>
2024-09-18 11:50:08 -0300

    feat: add new format import and call img icon

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"runtime"
@@ -9,59 +11,57 @@ import (
 	"github.com/getlantern/systray"
 )
 
+//go:embed golang.png
+var iconData []byte
 
 func main() {
 	fmt.Println("Hello, World!")
+	if iconData == nil {
+		log.Fatal("Erro: Não foi possível carregar o ícone")
+	}
 	systray.Run(onReady, onExit)
 }
 
 func onReady() {
-	systray.SetIcon(getIcon("/home/felipe/dev/go-process/golang.png"))
+	if len(iconData) == 0 {
+		log.Fatal("Erro: iconData está vazio")
+	}
+	systray.SetIcon(iconData)
 	systray.SetTitle("Meu programa Go")
 	systray.SetTooltip("Exemplo de app Go com icone na bandeja do sistema")
 	mQuit := systray.AddMenuItem("Sair", "Sair do programa")
 
-    // https url
-    mOpenWeb := systray.AddMenuItem("Abrir Site", "Abre o site oficial")
-    go func() {
-        for {
-            <-mOpenWeb.ClickedCh
-            // Em cima do Println podemos deixar o comando para abrir o navegador com a url desejada.
-            fmt.Println("Abrindo site...")
-        }
-    }()
+	// https url
+	mOpenWeb := systray.AddMenuItem("Abrir Site", "Abre o site oficial")
+	go func() {
+		for {
+			<-mOpenWeb.ClickedCh
+			// Em cima do Println podemos deixar o comando para abrir o navegador com a url desejada.
+			fmt.Println("Abrindo site...")
+		}
+	}()
 
-    // notificação
-    mNotify := systray.AddMenuItem("Notificar", "Mostra uma notificação")
-    go func() {
-        for {
-            <-mNotify.ClickedCh
-            // Deixa aqui para caso queira implementar uma notificação com a biblioteca do sistema operacional.
-            fmt.Println("Mostrando notificação...")
-        }
-    }()
+	// notificação
+	mNotify := systray.AddMenuItem("Notificar", "Mostra uma notificação")
+	go func() {
+		for {
+			<-mNotify.ClickedCh
+			// Deixa aqui para caso queira implementar uma notificação com a biblioteca do sistema operacional.
+			fmt.Println("Mostrando notificação...")
+		}
+	}()
 
 	go func() {
-    <-mQuit.ClickedCh
-    fmt.Print("Saindo...")
-    systray.Quit()
+		<-mQuit.ClickedCh
+		fmt.Print("Saindo...")
+		systray.Quit()
 
-    os.Exit(0)
+		os.Exit(0)
 	}()
 }
 
 func onExit() {
 	// Limpar
-}
-
-func getIcon(path string) []byte {
-	// carregar icone
-	b, err := os.ReadFile(path)
-	if err != nil {
-		fmt.Println("Não foi possivel carregar o icone:",err)
-		return nil
-	}
-	return b
 }
 
 func openBrowser(url string) {
