f4db80a33d32c34cd46b3c5783de40dc5f568012
felipethecosta <felipethecosta@gmail.com>
2024-09-18 09:32:58 -0300

    feat: add new commands

--- main.go
+++ main.go
@@ -19,11 +19,31 @@ func onReady() {
 	systray.SetTooltip("Exemplo de app Go com icone na bandeja do sistema")
 	mQuit := systray.AddMenuItem("Sair", "Sair do programa")
 
+    // https url
+    mOpenWeb := systray.AddMenuItem("Abrir Site", "Abre o site oficial")
+    go func() {
+        for {
+            <-mOpenWeb.ClickedCh
+            // Em cima do Println podemos deixar o comando para abrir o navegador com a url desejada.
+            fmt.Println("Abrindo site...")
+        }
+    }()
+
+    // notificação
+    mNotify := systray.AddMenuItem("Notificar", "Mostra uma notificação")
+    go func() {
+        for {
+            <-mNotify.ClickedCh
+            // Deixa aqui para caso queira implementar uma notificação com a biblioteca do sistema operacional.
+            fmt.Println("Mostrando notificação...")
+        }
+    }()
 
 	go func() {
     <-mQuit.ClickedCh
     fmt.Print("Saindo...")
     systray.Quit()
+
     os.Exit(0)
 	}()
 }
